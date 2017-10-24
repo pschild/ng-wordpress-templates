bd7c5d7ae632554d268a5ec1be6def7f9ad024a2
pschild <philippe.schild@gmail.com>
2017-10-24 12:08:05 +0200

    added services

--- ng-wordpress-templates/src/services/ng-wordpress-services.module.ts
+++ ng-wordpress-templates/src/services/ng-wordpress-services.module.ts
@@ -2,10 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {HttpModule} from "@angular/http";
 import {TimelineService} from "./timeline.service";
-import {NGWT_CONFIG, NgwtConfig} from "../config/NgwtConfig";
 import {PostService} from "./post.service";
 import {ProjectService} from "./project.service";
 import {MediaService} from "./media.service";
+import {NavigationService} from "./navigation.service";
+import {PageService} from "./page.service";
+import {NGWT_CONFIG, NgwtConfig} from "../config/NgwtConfig";
 
 @NgModule({
     imports: [
@@ -24,12 +26,12 @@ export class NgWordpressServicesModule {
                     provide: NGWT_CONFIG,
                     useValue: config
                 },
-                // PageService,
+                PageService,
                 PostService,
                 ProjectService,
                 TimelineService,
-                MediaService
-                // NavigationService
+                MediaService,
+                NavigationService
             ]
         };
     }
